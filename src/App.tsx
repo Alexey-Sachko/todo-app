727085d0c44f3be0529e89db550d2395ec70ac73
Alexey Sachko <alexey.sachko@gmail.com>
2020-08-18 17:53:57 +0500

    Получение постов через effector

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import PostsList from "./components/PostsList";
+import { PostsGate } from "./components/PostsList/PostsList";
 
 function App() {
   return (
     <div>
       <PostsList />
+      <PostsGate />
     </div>
   );
 }
--- src/components/PostsList/PostsList.tsx
+++ src/components/PostsList/PostsList.tsx
@@ -1,15 +1,49 @@
 import React from "react";
-import { postList } from "../../data/posts-list";
+import { createStore, createEffect, forward } from "effector";
+import { useStore, createGate } from "effector-react";
+
 import Post from "../Post/Post";
+import { IPost } from "../../types/post.type";
+
+const getPostFx = createEffect({
+  async handler() {
+    const req = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    return req.json();
+  },
+});
+
+const $posts = createStore<IPost[]>([]).on(
+  getPostFx.doneData,
+  (_, result) => result
+);
+
+export const PostsGate = createGate("gate with props");
+
+forward({ from: PostsGate.state, to: getPostFx });
+
+PostsGate.open.watch(() => {
+  console.log("mounted");
+});
+
+PostsGate.close.watch(() => {
+  console.log("unmounted");
+});
 
 const PostsList = () => {
+  const posts = useStore($posts);
+  const loading = useStore(getPostFx.pending);
+
   return (
     <div>
-      <ul>
-        {postList.map((post) => (
-          <Post key={post.id} data={post} />
-        ))}
-      </ul>
+      {loading ? (
+        "loading"
+      ) : (
+        <ul>
+          {posts.map((post) => (
+            <Post key={post.id} data={post} />
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
