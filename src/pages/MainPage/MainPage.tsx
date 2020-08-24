a8f0d9bba9669941699de3aff68a744f29817585
Alexey Sachko <alexey.sachko@gmail.com>
2020-08-24 17:04:28 +0500

    page layout

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./MainPage.module.css";
 
 const MainPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <header className={styles.header}>Drag and Drop Demo</header>
+      <div className={styles.content}>
+        <div className={styles.leftSide}>
+          <h5 className={styles.colHeader}>TODO</h5>
+        </div>
+        <div className={styles.middleSide}></div>
+        <div className={styles.rightSide}>
+          <h5 className={styles.colHeader}>Completed</h5>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MainPage;
