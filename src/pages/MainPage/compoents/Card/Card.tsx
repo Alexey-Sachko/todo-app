1614f81d771230d5e26f4aaa14eadc026ad533f1
Ilya Agarishev <ilya.business.avtocode@gmail.com>
2020-08-25 10:09:11 +0500

    draggable

--- src/pages/MainPage/compoents/Card/Card.tsx
+++ src/pages/MainPage/compoents/Card/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Card.module.css";
 
 type CardProps = {
@@ -6,7 +6,57 @@ type CardProps = {
 };
 
 const Card = ({ title }: CardProps) => {
-  return <div className={styles.container}>{title}</div>;
+  const draggableRef = useRef<HTMLDivElement | null>(null);
+  const [isActive, setIsActive] = useState(false);
+
+  const mouseDownHandler = (e: React.MouseEvent) => {
+    setIsActive(true);
+    const dragEl = draggableRef.current;
+    let xOffset = 0;
+    let yOffset = 0;
+
+    if (dragEl) {
+      xOffset = e.clientX - dragEl.offsetLeft;
+      yOffset = e.clientY - dragEl.offsetTop;
+      setCoords(dragEl, e.clientX - xOffset, e.clientY - yOffset);
+    }
+
+    const mouseMoveHandler = (e: MouseEvent) => {
+      if (dragEl) {
+        const { clientX, clientY } = e;
+        setCoords(dragEl, clientX - xOffset, clientY - yOffset);
+      }
+    };
+
+    const mouseUpHandler = (e: MouseEvent) => {
+      setIsActive(false);
+      document.removeEventListener("mouseup", mouseUpHandler);
+      document.removeEventListener("mousemove", mouseMoveHandler);
+    };
+
+    document.addEventListener("mousemove", mouseMoveHandler);
+    document.addEventListener("mouseup", mouseUpHandler);
+  };
+
+  return (
+    <div className={styles.shadow}>
+      <div
+        ref={draggableRef}
+        className={styles.container}
+        onMouseDown={mouseDownHandler}
+        style={{
+          position: isActive ? "fixed" : "static",
+        }}
+      >
+        {title} {isActive && "active"}
+      </div>
+    </div>
+  );
 };
 
 export default Card;
+
+function setCoords(el: HTMLElement, x: number, y: number) {
+  el.style.left = `${x}px`;
+  el.style.top = `${y}px`;
+}
